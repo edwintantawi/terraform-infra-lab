e52d9674391f718a92a36078f0cb34a722140da7
edwintantawi <edwintantawi.workspace@gmail.com>
2023-04-23 17:05:08 +0700

    feat: add google provider

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.62.1"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+}
